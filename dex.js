d0e441d130bca155e41fb519e435ae1653c78cf1
pavankumarkoda <148906137+pavankumarkoda@users.noreply.github.com>
2024-02-17 23:39:39 +0530

    Add files via upload

---
+++ dex.js
@@ -0,0 +1,39 @@
+let username="";
+
+// username=username.trim();
+// let letter= username.charAt(0);
+// letter=letter.toUpperCase();
+// let extrachars=username.slice(1);
+// extrachars=extrachars.toLowerCase();
+
+
+// username=letter+extrachars;
+// console.log(username);
+
+
+while(username === "" || username !== "yes"|| username === null){
+   username= window.prompt("Will you love me type (yes/no)");
+}
+ alert("i love you tooooooooooooooooo");
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
